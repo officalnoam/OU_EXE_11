82c4a54ef9223c00d9318f1fbb464da4283b580c
Noam <noamrooffical@gmail.com>
2025-03-19 13:42:15 +0200

    Add palindrome utils

---
+++ palindrome/palindrome_utils.c
@@ -0,0 +1,21 @@
+#include "palindrome_utils.h"
+#include <string.h>
+
+/*
+The function will check if a string is a palindrome or not. This includes whitespaces in the string.
+
+Input-
+  char* str: The string being checked.
+
+Output-
+  bool: true if the string is a palindrome false otherwise.
+*/
+bool check_if_string_is_palindrome(char* str) {
+  int i;
+  for (i=0; i <= strlen(str) / 2; i ++)
+  {
+    if (str[i] != str[strlen(str)-i-1])
+      return false;
+  }
+  return true;
+}
---
+++ palindrome/palindrome_utils.h
@@ -0,0 +1,7 @@
+#include "boolean.h"
+
+#ifndef PALINDROME_UTILS
+  #define PALINDROME_UTILS
+  
+  bool check_if_string_is_palindrome(char* str);
+#endif
